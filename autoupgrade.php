587bb63db6aa8e07b513fee99ee2d2e7d16e6625
Thomas Nabord <thomas.nabord@prestashop.com>
2025-11-25 17:25:47 +0000

    Changes

--- autoupgrade.php
+++ autoupgrade.php
@@ -30,6 +30,8 @@ class Autoupgrade extends Module
      */
     protected $container;
 
+    // This PR does not contain any changes about a migration script -> No label is expected.
+
     public function __construct()
     {
         $this->name = 'autoupgrade';
